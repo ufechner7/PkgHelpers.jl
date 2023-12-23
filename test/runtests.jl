d0302e1799d01a4fee8c81ff50a53e5b915c2c22
KeithWM <keith@myerscough.nl>
2023-12-23 21:25:23 +0100

    Stricter testing of the TOML, checking equality line by line.
    Add TOML where the order is completely wrong as a test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,19 +12,13 @@ function comp(vec1, vec2, n)
 end
 
 function tomlcmp(path1::AbstractString, path2::AbstractString)
-    dict1 = TOML.parsefile(path1)
-    dict2 = TOML.parsefile(path2)
-    for key in keys(dict1)
-        if repr(dict1[key]) != repr(dict2[key])
-            return false
+    open(path1) do f1
+        open(path2) do f2
+            for (l1, l2) in zip(readlines(f1), readlines(f2))
+                @test l1 == l2
+            end
         end
     end
-    for key in keys(dict2)
-        if repr(dict1[key]) != repr(dict2[key])
-            return false
-        end
-    end
-    true
 end
 
 st = "Project PkgHelpers v0.1.0\nStatus `~/repos/PkgHelpers.jl/Project.toml`\n  [44cfe95a] Pkg v1.10.0\n  [fa267f1f] TOML v1.0.3\n"
@@ -37,11 +31,11 @@ st = "Project PkgHelpers v0.1.0\nStatus `~/repos/PkgHelpers.jl/Project.toml`\n  
     mydir = mktempdir()
     filename = "test-1.toml"
     filename2 = "test-2.toml"
-    mytoml = joinpath(mydir, filename)
+    @show mytoml = joinpath(mydir, filename)
     cp(filename, mytoml, force=true)
     chmod(mytoml, 0o777)
     PkgHelpers.freeze1(nothing; julia="~1.10", status=st, mytoml=mytoml)
-    @test tomlcmp(filename2, mytoml)
+    tomlcmp(filename2, mytoml)
 end
 @testset "freeze - relaxed" begin
     project_file, compat = PkgHelpers.project_compat(Pkg, true, false; status=st)
@@ -55,7 +49,7 @@ end
     cp(filename, mytoml, force=true)
     chmod(mytoml, 0o777)
     PkgHelpers.freeze1(nothing; julia="~1.10", relaxed=true, status=st, mytoml=mytoml)
-    @test tomlcmp(filename2, mytoml)
+    tomlcmp(filename2, mytoml)
 end
 @testset "lower_bound" begin
     project_file, compat = PkgHelpers.project_compat(Pkg, false, false; status=st)
@@ -69,7 +63,7 @@ end
     cp(filename, mytoml, force=true)
     chmod(mytoml, 0o777)
     PkgHelpers.freeze1(nothing; julia="1.6", lowerbound=true, status=st, mytoml=mytoml)
-    @test tomlcmp(filename2, mytoml)
+    tomlcmp(filename2, mytoml)
 end
 @testset "lower_bound - relaxed" begin
     project_file, compat = PkgHelpers.project_compat(Pkg, false, false; status=st)
@@ -83,5 +77,19 @@ end
     cp(filename, mytoml, force=true)
     chmod(mytoml, 0o777)
     PkgHelpers.freeze1(nothing; julia="1.6", relaxed=true, lowerbound=true, status=st, mytoml=mytoml)
-    @test tomlcmp(filename2, mytoml)
+    tomlcmp(filename2, mytoml)
+end
+@testset "order" begin
+    project_file, compat = PkgHelpers.project_compat(Pkg, false, false; status=st)
+    @test basename(project_file) == "Project.toml"
+    @test haskey(compat, "TOML")
+    @test haskey(compat, "Pkg")
+    mydir = mktempdir()
+    filename = "test-unordered.toml"
+    filename2 = "test-2.toml"
+    mytoml = joinpath(mydir, filename)
+    cp(filename, mytoml, force=true)
+    chmod(mytoml, 0o777)
+    PkgHelpers.freeze1(nothing; julia="~1.10", status=st, mytoml=mytoml)
+    tomlcmp(filename2, mytoml)
 end
